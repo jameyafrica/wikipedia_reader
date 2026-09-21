feb39fe10910b9dd582f910467c83977feff52f6
Jamey <jaafrcpt025@student.wethinkcode.co.za>
2026-09-21 15:54:09 +0200

    feat: completed chapter 11(make http requests)

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
-// import 'summary.dart';
+import 'summary.dart';
 
 void main() {
   runApp(const MainApp());
@@ -25,3 +25,24 @@ class MainApp extends StatelessWidget {
     );
   }
 }
+
+class ArticleModel {
+  Future<Summary> getRandomArticleSummary() async {
+    final uri = Uri.https(
+      'en.wikipedia.org',
+      '/api/rest_v1/page/random/summary',
+    );
+    final response = await get(uri);
+
+    if (response.statusCode != 200) {
+      throw const HttpException('Failed to update resource');
+    }
+
+    return Summary.fromJson(jsonDecode(response.body) as Map<String, Object?>);
+  }
+}
+
+
+
+
+
